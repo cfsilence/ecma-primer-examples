8b9f4f6a8995329e1bcd93f4c4fb762fd5824aad
toddsharp <toddrsharp@gmail.com>
2018-05-14 23:01:04 -0400

    fixed formatting

--- examples/async-await/async-await-1.js
+++ examples/async-await/async-await-1.js
@@ -2,11 +2,12 @@ async function a() {
     let promise = new Promise((resolve, reject) => {
       setTimeout(() => resolve("done!"), 1000)
     });
-  
     const result = await promise; // wait till the promise resolves (*)
     console.log(result);
     return result;
 }
 
+console.log('call it -->');
 a();
+console.log('it was called!');
    
