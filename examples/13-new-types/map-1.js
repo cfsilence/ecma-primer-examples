54d33fe976be0db0ead88ffcbad26430a9469423
toddsharp <toddrsharp@gmail.com>
2018-05-28 12:54:35 -0400

    add map/set examples

---
+++ examples/13-new-types/map-1.js
@@ -0,0 +1,23 @@
+/* Map */
+const m = new Map();
+m.set('key', 'value');
+
+const arr = [1,2,3];
+m.set(arr, 'another value')
+console.log('map-->', m);
+console.log('check key exists-->', m.has(arr));
+console.log('get a value by key-->', m.get('key'));
+console.log('delete an entry by key-->', m.delete('key'));
+console.log('map-->', m);
+console.log('get amount of entries in map-->', m.size);
+/* clear all entries */
+m.clear();
+console.log('map-->', m);
+
+m.set('key1', 'value1');
+m.set('key2', 'value2');
+m.set('key3', 'value3');
+
+m.forEach((value, key) => {
+    console.log(`${key} = ${value}`);
+})
---
+++ examples/13-new-types/set-2.js
@@ -0,0 +1,16 @@
+/* Set */
+const s = new Set();
+s.add(1);
+s.add({id: 1});
+s.add([1,2,3]);
+
+console.log(s);
+console.log('set size-->', s.size);
+console.log('set values-->', s.values());
+s.delete(1);
+console.log(s);
+
+let i = 0;
+s.forEach( (value) => {
+    console.log(`${i++}-->`, value);
+})
